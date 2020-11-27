868e8ca1e5cefa746a52083b20202e764b7b6c63
Laurancy Dorian <laurancy.dorian@gmail.com>
2020-11-27 12:07:31 +0100

    Updated KafkaService for json format in pipeline

--- src/main/java/polytech/adce/geolocation/services/KafkaService.java
+++ src/main/java/polytech/adce/geolocation/services/KafkaService.java
@@ -2,6 +2,8 @@ package polytech.adce.geolocation.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.JsonParser;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import polytech.adce.geolocation.models.Geolocation;
@@ -12,6 +14,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 
 @Component
@@ -41,19 +44,41 @@ public class KafkaService {
     // --------------------------------------------------
 
 
+//    @KafkaListener( groupId = "springConsumer", topics = "positions")
+//    public void consume(String loc) {
+//        System.out.println(loc);
+//        String[] locations = loc.split(":");
+//
+//        Geolocation geoloc = new Geolocation();
+//        try {
+//            geoloc.setUsername(locations[0]);
+//            geoloc.setLatitude(Double.parseDouble(locations[1]));
+//            geoloc.setLongitude(Double.parseDouble(locations[2]));
+//
+//            DateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);;
+//            Date date = (Date)formatter.parse(locations[3] + ":" + locations[4] + ":" + locations[5]);
+//            geoloc.setGeolocation_timestamp(date);
+//
+//            System.out.println(geoloc);
+//
+//            geolocationService.processLocation(geoloc);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     @KafkaListener( groupId = "springConsumer", topics = "positions")
     public void consume(String loc) {
-        System.out.println(loc);
-        String[] locations = loc.split(":");
-
+        JsonParser parser = new BasicJsonParser();
+        Map<String, Object> m = parser.parseMap(loc);
         Geolocation geoloc = new Geolocation();
         try {
-            geoloc.setUsername(locations[0]);
-            geoloc.setLatitude(Double.parseDouble(locations[1]));
-            geoloc.setLongitude(Double.parseDouble(locations[2]));
+            geoloc.setUsername(m.get("username").toString());
+            geoloc.setLatitude(Double.parseDouble(m.get("latitude").toString()));
+            geoloc.setLongitude(Double.parseDouble(m.get("longitude").toString()));
 
             DateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);;
-            Date date = (Date)formatter.parse(locations[3] + ":" + locations[4] + ":" + locations[5]);
+            Date date = (Date)formatter.parse(m.get("timestamp").toString());
             geoloc.setGeolocation_timestamp(date);
 
             System.out.println(geoloc);
@@ -63,4 +88,5 @@ public class KafkaService {
             e.printStackTrace();
         }
     }
+
 }
